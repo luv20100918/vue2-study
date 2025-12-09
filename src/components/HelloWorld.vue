74dea1d919ee855876d5c69a10531c5b4cb12d51
haeun <sabomdh123@gmail.com>
2025-12-09 21:08:14 +0900

    같은파일이니까 커밋해보자

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
     </p>
     <h3>유용한 링크</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Vue.js 공식 문서</a></li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Vue.jsㅇㅇㅇㅇ 공식 문서</a></li>
       <li><a href="https://cli.vuejs.org" target="_blank" rel="noopener">Vue CLI 문서</a></li>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Vue Router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Vuex</a></li>
